f9b44395ada119e96c27130f311dd335a37c48b6
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2024-12-16 19:04:06 -0600

    added custom colors to tailwind.config.ts file

--- angel_undeveloped_website/tailwind.config.ts
+++ angel_undeveloped_website/tailwind.config.ts
@@ -16,3 +16,34 @@ export default {
   },
   plugins: [],
 } satisfies Config;
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#0a0a0a',
+        light: '#f5f5f5',
+        neutral: {
+          400: '#a3a3a3',
+          600: '#525252',
+          800: '#27272a',
+        },
+        slate: {
+          600: '#475569',
+          800: '#1e293b',
+        },
+        rose: '#e11d48',
+        violet: '#9333ea',
+        indigo: '#4f46e5',
+        blue: '#2563eb',
+      },
+    },
+  },
+  plugins: [],
+};
